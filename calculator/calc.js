c7bb7d882ecac4df6a74147a39b71f7218ac4280
Sasha <sashaf708@gmail.com>
2024-06-27 13:04:28 +0400

    заебалась

--- calculator/calc.js
+++ calculator/calc.js
@@ -2,11 +2,10 @@ $(function () {
   initCalc();
   calcKredit();
   //something
-
+  //изменения от васильны
   //comment
-  //privet
-
   //comment23232
+  //ponos
   $("#autoCost_val").focusin(function () {
     var val_autoCost = $("#autoCost_val").val().replace(/\D+/g, "");
     var min_autoCost = $("#autoCost").attr("min-value-data");
