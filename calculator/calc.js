d6beb274f400677ea11797cfd1be7b0d910d5564
Sasha <sashaf708@gmail.com>
2024-07-29 10:39:20 +0400

    добавила ползунок

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,15 +1,12 @@
 // калькулятор
 function calculate() {
 	var amount = parseInt($("#deposit_val").val().replace(/\s/g, ""), 10);
-	var term = parseInt($("#term").val(), 10);
+	var term = parseInt($("#term_val").val(), 10);
 	var rate = checkRate(term);
 	var daysInYear = new Date().getFullYear() % 4 === 0 ? 366 : 365;
 	var daysInTerm = (91 * term) / 3;
-
 	var income = ((amount * rate) / daysInYear) * daysInTerm;
-
-	var termText = term === 3 ? "месяца" : "месяцев";
-	$("#months").text(term + " " + termText);
+	$("#months").text(term + " " + formatMonth(term));
 	$("#rate").text(formatNumber(rate * 100) + "%");
 	$("#finalAmount").text(formatMoney((amount + income).toFixed(0)));
 	$("#income").text(formatMoney(income.toFixed(0)));
@@ -32,8 +29,17 @@ function formatNumber(n) {
 function formatMoney(amount) {
 	return amount.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
-
-// ползунок
+// код формата вывода месяцев
+function formatMonth(number) {
+	var cases = [2, 0, 1, 1, 1, 2];
+	var titles = ["месяц", "месяца", "месяцев"];
+	return titles[
+		number % 100 > 4 && number % 100 < 20
+			? 2
+			: cases[number % 10 < 5 ? number % 10 : 5]
+	];
+}
+// ползунок суммы
 $(function () {
 	// вывод суммы с пробелами
 	function formatMoney(number) {
@@ -43,7 +49,7 @@ $(function () {
 	$("#autoCost").slider({
 		range: "min",
 		value: 5000,
-		min: 5000,
+		min: 5000, //минимальная сумма взноса
 		max: 5000000,
 		step: 500,
 		slide: function (event, ui) {
@@ -69,4 +75,36 @@ $(function () {
 
 	$("#term").change(calculate);
 	calculate();
+});
+
+// ползунок срока
+$(function () {
+	$("#autoTerm").slider({
+		range: "min",
+		value: 3,
+		min: 3, //минимальный срок
+		max: 24,
+		step: 3,
+
+		slide: function (event, ui) {
+			$("#term_val").val(ui.value + " " + formatMonth(ui.value)); // Обновление поля ввода при изменении ползунка
+			calculate();
+		},
+	});
+
+	$("#term_val").change(function () {
+		var value = $(this).val().replace(/\D/g, ""); // Удаление нечисловых символов
+		value = parseInt(value, 10); // Преобразование строки в число
+		if (value >= 3 && value <= 24) {
+			value -= value % 3;
+			$("#autoTerm").slider("value", value); // Обновление ползунка при изменении поля ввода
+			$(this).val(value + " " + formatMonth(value));
+			calculate();
+		} else {
+			value = value < 3 ? 3 : 24;
+			$("#autoTerm").slider("value", value);
+			$(this).val(value + " " + formatMonth(value));
+			calculate();
+		}
+	});
 });
